13e864698a20127efbf4af7888d5aae9443a9a7e
footgrammer <junseok0211@nsu.ac.kr>
2025-04-10 09:52:58 +0900

    2025-04-10

---
+++ 2025-04-10-count-bit.dart
@@ -0,0 +1,14 @@
+List<int> countBits(int n) {
+  List<int> dp = List.filled(n + 1, 0);
+
+  for (int i = 1; i <= n; i++) {
+    dp[i] = dp[i >> 1] + (i & 1);
+  }
+
+  return dp;
+}
+
+void main() {
+  int num = 2;
+  print(countBits(num));
+}
