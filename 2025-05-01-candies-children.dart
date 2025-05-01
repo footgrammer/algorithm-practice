ff9dedf5336d7e39e866da0c8fe4702a47d8115a
footgrammer <junseok0211@nsu.ac.kr>
2025-05-01 09:42:08 +0900

    feat : 2025-05-01

---
+++ 2025-05-01-candies-children.dart
@@ -0,0 +1,23 @@
+List<bool> Solution(List<int> candies, int extraCandies) {
+  int maxCandy = 0;
+  for (int candy in candies) {
+    if (candy > maxCandy) maxCandy = candy;
+  }
+  // maxCandy = candies.reduce(a, b => a > b ? a : b)
+  List<bool> possibilityList = [];
+  for (int candy in candies) {
+    if ((candy + extraCandies) >= maxCandy) {
+      possibilityList.add(true);
+    } else {
+      possibilityList.add(false);
+    }
+  }
+
+  return possibilityList;
+}
+
+void main() {
+  List<int> candies = [2, 3, 5, 4, 6];
+  int extraCandies = 3;
+  print(Solution(candies, extraCandies));
+}
